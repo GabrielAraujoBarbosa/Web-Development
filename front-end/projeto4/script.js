7cfbd20b80ddf318f0e289e00e9e9a96d61e5f9b
Gabriel <ogabriel.araujo2@gmail.com>
2026-03-18 16:49:29 -0300

    projeto4 finalizado

--- front-end/projeto4/script.js
+++ front-end/projeto4/script.js
@@ -1,23 +1,27 @@
+/* Default Login */
 let defaultUser = "admin";
 let defaultPassword = 1234;
 
+/* DOM Elements */
 const errorIcon = document.getElementById('errorIcon');
 const sucessIcon = document.getElementById('sucessIcon');
 const form = document.querySelector('form');
+let inputUser = document.getElementById('textBox')
+let inputPassword = document.getElementById('passwordBox');
 
+/* Event Listeners (form)*/
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    
-    let typedUser = document.getElementById('textBox').value;
-    let typedPassword = document.getElementById('passwordBox').value;
+    let typedUser = inputUser.value;
+    let typedPassword = inputPassword.value;
     
     const loginResponse = document.getElementById('statusLogin');
     
     if (typedUser == defaultUser && typedPassword == defaultPassword) {
         errorIcon.classList.add('hideElement');
         sucessIcon.classList.remove('hideElement');
-        loginResponse.textContent = "Você fez login com sucesso! Seja bem-vindo, admin.";
+        loginResponse.innerHTML = "Você fez login com sucesso! Seja bem-vindo, <b>administrador</b>.";
     }
     else {
         sucessIcon.classList.add('hideElement');
@@ -25,3 +29,15 @@ form.addEventListener('submit', (event) => {
         loginResponse.textContent = "As informações de login que você inseriu estão incorretas."
     }
 });
+form.addEventListener('reset', (event) => {
+    event.preventDefault();
+
+    errorIcon.classList.add('hideElement');
+    sucessIcon.classList.add('hideElement');
+    const loginResponse = document.getElementById('statusLogin');
+    loginResponse.textContent = "";
+    inputUser.value = "";
+    inputPassword.value = "";
+});
+
+
